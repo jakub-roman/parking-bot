c5daaef6a9fdaa89f76e0c2682c0161b72ea5334
jakub-roman <kuba@jeer.cz>
2022-08-19 13:56:42 +0200

    name convention

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm'
 export const db = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
+  port: parseInt(process.env.POSTGRES_PORT || '5432'),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB || 'parking-bot',
